c5835966afe32caaae6d8659dd38fe169d5e4aa0
alejandro <alejandroaciatti@gmail.com>
2014-05-22 23:28:29 -0300

    templating initialized, all system nominal. wipu-sucus-uasu. /ponies

--- app/models/Startup.scala
+++ app/models/Startup.scala
@@ -1,6 +1,7 @@
 package models
 
 import anorm.{NotAssigned, Pk}
+import play.api.libs.json.Json
 
 /**
  * Created by Javi on 5/15/14.
@@ -9,6 +10,13 @@ case class Startup(id: Pk[Long] = NotAssigned, name: String) {
 
 }
 
-class Location(id: Pk[Long] = NotAssigned, name: String) {
+case class Location(id: Pk[Long] = NotAssigned, name: String, angelId:Long) {
 
+}
+
+object Location{
+
+  def mockLocations = {
+    Seq(new Location(NotAssigned,"Argentina", 1613), new Location(NotAssigned,"Brazil",1622))
+  }
 }
