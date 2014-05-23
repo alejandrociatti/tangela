e90b3a3652a573c5e804a06c83e21d29e4f2fc62
Martin Gutierrez <gutix007@hotmail.com>
2014-05-22 23:33:32 -0300

    primer intento de buscar por location

---
+++ app/controllers/Location.scala
@@ -0,0 +1,38 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+import scala.concurrent.{ExecutionContext, Future}
+import play.api.libs.ws.WS
+import play.api.libs.ws
+import ExecutionContext.Implicits.global
+
+
+/**
+ * User: Martin Gutierrez
+ * Date: 22/05/14
+ * Time: 20:52
+ */
+object Location extends Controller{
+
+  def getCountryIdByName(countryName:String):Future[ws.Response] = {
+    WS.url("https://api.angel.co/1/search?type=LocationTag&query=" + countryName).get()
+  }
+
+  def getCountryChildren(countryName:String) = Action {
+    val promiseOfCountryId:Future[ws.Response] = getCountryIdByName(countryName)
+
+    Async{
+      promiseOfCountryId.map{response =>
+        val countryId = response.json.\\("id").head
+        Async {
+          WS.url("https://api.angel.co/1/tags/" + countryId + "/children").get().map { resp =>
+            val ids = resp.json.\\("id")
+            val names = resp.json.\\("display_name")
+
+            Ok()
+          }
+        }
+      }
+    }
+  }
+}
