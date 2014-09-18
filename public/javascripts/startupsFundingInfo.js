c9ed277fcc468b556f6eec8bb76a5fa7c922192f
alejandro <alejandroaciatti@gmail.com>
2014-09-18 17:44:25 -0300

    startups funding view

---
+++ public/javascripts/startupsFundingInfo.js
@@ -0,0 +1,121 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: alejandro
+ * Date: 18/09/14
+ * Time: 17:17
+ */
+
+
+angular.module('AAC', ['app.controllers']);
+
+var module = angular.module('app.controllers', ['app.services']);
+
+module.controller('startupsFundingInfo', ['$scope', 'dataAccess',
+        function ($scope, dataAccess) {
+
+            $scope.startupsResultsReached= true;
+            $scope.searching= false;
+            $scope.optionSelectMsg = 'Search first.';
+            $scope.persons= [] ;
+
+            $scope.submit = function () {
+                $scope.optionSelectMsg = 'Loading results...';
+                $scope.startupsResultsReached= true;
+                $scope.searching= true;
+                dataAccess.getPeopleNetwork($scope.location, $('#creation-date').val(), $scope.market, $scope.quality, function(fundings){
+                    $scope.fundings= startups;
+                    $scope.searching= false;
+                    $scope.startupsResultsReached= fundings.length != 0;
+                    $scope.optionSelectMsg = 'Select a startup.';
+                    $scope.$apply();
+                });
+            };
+
+            $scope.exportCSV = function () {
+                var obj = {
+                    headers: ["id", "startup name","round type","amount", "closed at"],
+                    values: []
+                } ;
+                for (var i = 0; i < $scope.fundings.length; i++) {
+                    var fund = $scope.fundings[i];
+                    obj.values.push([fund.id, fund.name, fund.round_type, fund.amount, fund.closed_at]);
+                }
+
+                dataAccess.getCSV(JSON.stringify(obj), function(file){
+                    var pom = document.createElement('a');
+                    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(file));
+                    pom.setAttribute('download', 'data.csv');
+                    pom.click();
+                });
+            };
+
+
+
+
+            //Pagination control:
+            $scope.itemsPerPage = 5;
+            $scope.currentPage = 0;
+
+            $scope.range = function() {
+                var rangeSize = 5;
+                var ret = [];
+                var start;
+
+                start = $scope.currentPage;
+                if ( start > $scope.pageCount()-rangeSize ) {
+                    start = $scope.pageCount()-rangeSize+1;
+                }
+
+                for (var i=start; i<start+rangeSize; i++) {
+                    if(i >= 0) {
+                        ret.push(i);
+                    }
+                }
+                return ret;
+            };
+
+            $scope.prevPage = function() {
+                if ($scope.currentPage > 0) {
+                    $scope.currentPage--;
+                }
+            };
+
+            $scope.setPage = function(n) {
+                $scope.currentPage = n;
+            };
+
+            $scope.nextPage = function() {
+                if ($scope.currentPage < $scope.pageCount()) {
+                    $scope.currentPage++;
+                }
+            };
+
+            $scope.nextPageDisabled = function() {
+                return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+            };
+
+            $scope.prevPageDisabled = function() {
+                return $scope.currentPage === 0 ? "disabled" : "";
+            };
+
+            $scope.pageCount = function() {
+                return Math.ceil($scope.persons.length/$scope.itemsPerPage)-1;
+            };
+
+
+        }]
+);
+
+module.filter('offset', function() {
+    return function(input, start) {
+        start = parseInt(start, 10);
+        /**
+         * Changed input.slice(start); in case input is undefined.
+         * This 'offset' function is used on ng-repeat directives,
+         * to change the starting element of the repeat.
+         * In this startupsInfo, it is used on $scope.roles, which for some reason was undefined.
+         * TODO: Check why typeof($scope.roles) == undefined at any given moment.
+         */
+        return (input || []).slice(start);
+    };
+});
