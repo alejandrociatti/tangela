f3d5d100664abe3096e74be1ec32339fd0f38f8a
alejandro <alejandroaciatti@gmail.com>
2014-11-10 15:57:51 -0300

    working on a paginate directive, simplify & conquer.

---
+++ public/javascripts/angular/directive/paginate.js
@@ -0,0 +1,8 @@
+angular.module('aacPagination', []).directive('aacPaginate', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs){
+
+        }
+    }
+});
