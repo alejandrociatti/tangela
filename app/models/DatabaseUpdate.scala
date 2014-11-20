142b65d87c1b27124923e7d02e6342c4ebdcd47e
Martin Gutierrez <gutix007@hotmail.com>
2014-11-20 19:22:17 -0300

    Database update class

---
+++ app/models/DatabaseUpdate.scala
@@ -0,0 +1,11 @@
+package models
+
+import org.joda.time.DateTime
+
+/**
+ * User: Martin Gutierrez
+ * Date: 20/11/14
+ * Time: 19:21
+ */
+
+class DatabaseUpdate(guteDate: DateTime = DateTime.now, folder: String)
